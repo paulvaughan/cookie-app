9cefd2781ef8a2ec634c23f568886a514dea6d9f
Pablo <paulvaughan.dev@gmail.com>
2019-03-12 16:14:05 -0400

    updates to css, fonts and background image. JS updated

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
-#Cookie Project
-##An inventory control application for Salmon Cookies Inc.
+# Cookie Project: Code 201
+
+## An inventory control application for Salmon Cookies Inc.
 
 - Inclusion of a README file
 - Inclusion of HTML, CSS and JavaScript
 - Linking of all working files
+
+## Updates
+
+- Include JS code to calculate cookies sold per hour
+- Updated CSS with fonts and background
+- Notations added to README file
---
+++ cookies.js
@@ -0,0 +1,82 @@
+'use strict'
+
+let elBody = document.getElementById('myBod')
+
+let hours = ['6am', '7am', '8am', '9am', '10am', '11am']
+
+let stores = []
+
+let Pike = {
+  storeName: 'Pike',
+  minCustomers: 23,
+  maxCustomers: 65,
+  cookiesPerCustomer: 6,
+  cookiesSoldPerHour: function() {
+    let randomNumber = Math.ceil(Math.random() * (this.maxCustomers - this.minCustomers)+ this.minCustomers)
+    return randomNumber * this.cookiesPerCustomer
+  }
+}
+
+let Seatac = {
+  storeName: 'Seatac',
+  minCustomers: 3,
+  maxCustomers: 23,
+  cookiesPerCustomer: 2,
+  cookiesSoldPerHour: function() {
+    let randomNumber = Math.ceil(Math.random() * (this.maxCustomers - this.minCustomers) + this.minCustomers)
+    return randomNumber * this.cookiesPerCustomer
+  } 
+}
+
+let Seacenter = {
+  storeName: 'Seacenter',
+  minCustomers: 11,
+  maxCustomers: 38,
+  cookiesPerCustomer: 4,
+  cookiesSoldPerHour: function() {
+    let randomNumber = Math.ceil(Math.random() * (this.maxCustomers - this.minCustomers) + this.minCustomers)
+    return randomNumber * this.cookiesPerCustomer
+  }
+}
+
+let Capitol = {
+  storeName: 'Capitol',
+  minCustomers: 20,
+  maxCustomers: 38,
+  cookiesPerCustomer: 3,
+  cookiesSoldPerHour: function() {
+    let randomNumber = Math.ceil(Math.random() * (this.maxCustomers - this.minCustomers) + this.minCustomers)
+    return randomNumber * this.cookiesPerCustomer
+  }
+}
+
+let Alki = {
+  storeName: 'Alki',
+  minCustomers: 2,
+  maxCustomers: 16,
+  cookiesPerCustomer: 5,
+  cookiesSoldPerHour: function() {
+    let randomNumber = Math.ceil(Math.random() * (this.maxCustomers - this.minCustomers) + this.minCustomers)
+    return randomNumber * this.cookiesPerCustomer
+  }
+}
+
+stores.push('Pike', 'Seatac', 'Seacenter', 'Capitol', 'Alki')
+
+//Displaying store name in a h2 tag
+let elPikeTitle = document.createElement('h2')
+elBody.appendChild(elPikeTitle)
+elPikeTitle.innerText = Pike.storeName
+
+//Create a ul to display information on customer purchases
+let elList = document.createElement('ul')
+elBody.appendChild(elList)
+
+//For loop to populate li in the HTML
+for(let i = 0; i < hours.length; i++) {
+  console.log(hours[i], 'Total number of cookies', Pike.cookiesSoldPerHour())
+  let elListItem = document.createElement('li')
+  elList.appendChild(elListItem)
+  elListItem.innerText = hours[i] + ':' + Pike.cookiesSoldPerHour() + ' cookies sold'
+}
+
